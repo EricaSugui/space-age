08fb879680ed6da599fba43b09d4324ad574628b
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2022-10-09 23:48:20 -0300

    feat: add script to turn app functional

--- space-age.js
+++ space-age.js
@@ -3,7 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = (planet, seconds) => {
+const age = (planet, seconds) => {
   const secondsInEarthYears = seconds / 31557600;
   const whatPlanet = {
     mercury: 0.2408467,
@@ -18,3 +18,33 @@ export const age = (planet, seconds) => {
   const result = secondsInEarthYears / whatPlanet[planet];
   return parseFloat(result.toFixed(2));
 };
+
+const select = document.getElementById("select-planet");
+let selectValue = "mercury";
+const secondsInput = document.getElementById("seconds-input");
+const calcularButton = document.getElementById("calcular-button");
+const limparButton = document.getElementById("limpar-button");
+const resultado = document.getElementById("resultado");
+
+select.addEventListener("change", function () {
+  selectValue = select.value;
+});
+
+calcularButton.addEventListener("click", () => {
+  const currentPlanet = selectValue;
+  const currentSeconds = secondsInput.value;
+  const finalResult = age(currentPlanet, currentSeconds);
+  calcularButton.setAttribute("disabled", true);
+  limparButton.removeAttribute("disabled");
+  resultado.value = finalResult;
+  console.log(finalResult);
+  console.log(currentPlanet);
+});
+
+limparButton.addEventListener("click", () => {
+  limparButton.setAttribute("disabled", true);
+  calcularButton.removeAttribute("disabled");
+  calcularButton.innerText = "Calcular";
+  secondsInput.value = "";
+  resultado.value = "";
+});
